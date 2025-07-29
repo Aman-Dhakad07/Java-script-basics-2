cb5b0c76a0e16f670214f6430357db87f06e2c87
Aman-Dhakad07 <dhakadaman035@gmail.com>
2025-07-29 09:25:30 +0530

    Add files via upload
    This is a java-script file ,in which the output can be seen in the console , as a part of java-script basic building programme.

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log('Hello Jee');
+
+let lastName='Babbar';
+
+let firstName = new String('Love');
+
+let message="This is my first Message";
+
+let words = message.split('');
+
+console.log(words);
+
+
+let message1=
+`This
+is
+my 
+first
+message`;
+console.log(message1);
+
+
+let date= new Date();
+
+let date1=new Date('June 20 1998 07:15');
+
+let date2=new Date(1998,11,20,7);
+console.log(date2);
+
